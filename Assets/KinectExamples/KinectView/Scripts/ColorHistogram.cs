fe648dc3dc2939a82e81b1e9278716a3f30a1764
Alexa Siu <asiu3@gatech.edu>
2017-06-01 00:19:16 -0700

    removing the annoying Mac Icon things

--- Assets/KinectExamples/KinectView/Scripts/ColorHistogram.cs
+++ Assets/KinectExamples/KinectView/Scripts/ColorHistogram.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorHistogram : MonoBehaviour {
+using HoloProxies.Utils;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+namespace HoloProxies.Objects
+{
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public class ColorHistogram  {
+
+		public Vector2[] data_unnormalized;
+		public Vector2[] data_normalized;
+
+		public float[] posterior;
+
+		public int noBins, dim;
+
+		public ColorHistogram (int nbins) {
+		}
+
+		public void UpdateHistogram( ColorHistogram newHist, float rf, float rb ) {
+		}
+
+		public void BuildHistogram() {
+		}
+
+		public void BuildHistogramFromLabeledRGBD() {
+		}
+
+		public void updateHistogramFromLabeledRGBD(float rf, float rb)
+		{
+//			ISRHistogram* tmphist = new ISRHistogram(this->noBins);
+//			buildHistogramFromLabeledRGBD(inimg);
+//			updateHistogram(tmphist, rf, rb);
+		}
+
+		public void updateHistogramFromLabeledRGBD(float rf, float rb, Vector4 bb)
+		{
+//			ISRHistogram* tmphist = new ISRHistogram(this->noBins);
+//			buildHistogramFromLabeledRGBD(inimg,bb);
+//			updateHistogram(tmphist, rf, rb);
+		}
+
+
 	}
+
 }
