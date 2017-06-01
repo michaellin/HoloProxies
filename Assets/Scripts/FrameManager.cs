c59682f2d73dc6c15e0a08e841be4f7fb7b33183
Michael Lin <michaelv03@gmail.com>
2017-06-01 00:36:12 -0700

    saving FrameManager

--- Assets/Scripts/FrameManager.cs
+++ Assets/Scripts/FrameManager.cs
@@ -1,58 +1,240 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Windows.Kinect;
+using System.IO;
 
 using HoloProxies.Utils;
 
 namespace HoloProxies.Objects
 {
     /// <summary>
-    /// The frameRGBD class carries the intermediate data for a 
-    /// RGBD frame, including occlusion map and pointCloud.
-    /// TODO figure out if any of the TODOs here need to be addressed.
-    /// File from: ISRFrame.h
+	/// The FrameManager class interfaces with the Kinect
+    /// File from: ISRFrame.h + LowLevelEngine
     /// </summary>
-    public class frameRGBD
+    public class FrameManager
     {
-        public int height;
-        public int width;
+        public Vector4 boundingbox;
 
-        public Vector2i depth_size;
-        public Vector2i rgb_size;
+        public int DownsampleSize = 2;
 
-        //TODO ISRView* view; Do we need this?
+        public int Width { get; private set; }
+        public int Height { get; private set; }
 
-        public histogramRGB hist;
-        public Texture2D ptCloud; // 2D point cloud of format RGBAFloat
-		public Texture2D rgbd;    // 2D rgbd of format RGBAFloat
+        public int ColorWidth { get; private set; }
+        public int ColorHeight { get; private set; }
 
-		// from hierarchy image levels
-        //public Vector4 boundingbox;
-        //public Vector4 intrinsic;
+        public int DepthWidth { get; private set; }
+        public int DepthHeight { get; private set; }
 
-        //TODO ISRImageHierarchy needed?
-        //TODO image levels needed?
+        public CameraSpacePoint[] Camera3DPoints { get; private set; }
+        public float[] PfVec { get; private set; }
+        public Texture2D PfImage { get; private set; }
 
-        //TODO VisualizationState needed?
+        public Texture2D ColorTexture { get; private set; }
+        public ColorSpacePoint[] ColorPoints { get; private set; }
+        public byte[] ColorData { get; private set; }
 
-        //TODO need calib to be its own file?
+        public ushort[] DepthData { get; private set; }
 
-        public frameRGBD( Vector2i color_size, Vector2i d_size )
+        private KinectSensor _Sensor;
+        private MultiSourceFrameReader _Reader;
+        private CoordinateMapper _Mapper;
+
+        private ColorHistogram histogram;
+
+        // Constructor
+        public FrameManager( ColorHistogram hist )
         {
-            depth_size = d_size;
-            rgb_size = color_size;
-            ptCloud = new Texture2D( d_size.x, d_size.y, TextureFormat.RGBAFloat, false );
-            rgbd = new Texture2D( d_size.x, d_size.y, TextureFormat.RGBAFloat, false );
+            _Sensor = KinectSensor.GetDefault();
+
+            if (_Sensor != null)
+            {
+                // Save histogram
+                histogram = hist;
+
+                _Mapper = _Sensor.CoordinateMapper;
+
+                _Reader = _Sensor.OpenMultiSourceFrameReader( FrameSourceTypes.Color | FrameSourceTypes.Depth );
+
+                var colorFrameDesc = _Sensor.ColorFrameSource.CreateFrameDescription( ColorImageFormat.Rgba );
+                ColorWidth = colorFrameDesc.Width;
+                ColorHeight = colorFrameDesc.Height;
+                ColorData = new byte[colorFrameDesc.BytesPerPixel * colorFrameDesc.LengthInPixels];
+                ColorTexture = new Texture2D( ColorWidth, ColorHeight, TextureFormat.RGBA32, false );
+
+                var depthFrameDesc = _Sensor.DepthFrameSource.FrameDescription;
+                DepthWidth = depthFrameDesc.Width;
+                DepthHeight = depthFrameDesc.Height;
+                DepthData = new ushort[depthFrameDesc.LengthInPixels];
+
+                // TODO
+                // Downsample all data frames if necessary
+
+                // Set FrameWidth and FrameHeight to the downsampled size
+                Width = DepthWidth;
+                Height = DepthHeight;
+
+                PfImage = new Texture2D( Width, Height, TextureFormat.RGBA32, false );
+
+                // Set buffers to align depth data to RGB and to align camera points
+                ColorPoints = new ColorSpacePoint[DepthWidth * DepthHeight];
+                Camera3DPoints = new CameraSpacePoint[DepthWidth * DepthHeight];
+                PfVec = new float[Width * Height];
+
+                if (!_Sensor.IsOpen)
+                {
+                    _Sensor.Open();
+                }
+            }
         }
 
-		public void GetRGBD ( out Texture2D rgbdTexture ) {
-			rgbdTexture = rgbd;
-		}
+        public void UpdateFrame()
+        {
 
-		public void MapRGBtoRGBD ( out UInt16[] LookupTable ) {
-			
-		}
+            if (_Reader != null)
+            {
+                var frame = _Reader.AcquireLatestFrame();
+                if (frame != null)
+                {
+                    var colorFrame = frame.ColorFrameReference.AcquireFrame();
+                    if (colorFrame != null)
+                    {
+                        var depthFrame = frame.DepthFrameReference.AcquireFrame();
+                        if (depthFrame != null)
+                        {
+                            colorFrame.CopyConvertedFrameDataToArray( ColorData, ColorImageFormat.Rgba );
+                            ColorTexture.LoadRawTextureData( ColorData );
+                            ColorTexture.Apply();
 
+                            depthFrame.CopyFrameDataToArray( DepthData );
+
+                            depthFrame.Dispose();
+                            depthFrame = null;
+
+                            // Map depth to RGBD
+                            AlignRGBD();
+
+                            // Unproject to 3D points in camera space
+                            PreparePointCloud();
+
+                        }
+
+                        colorFrame.Dispose();
+                        colorFrame = null;
+                    }
+
+                    frame = null;
+                }
+            }
+
+        }
+
+        // Creates a lookup list ColorPoints that map DepthData to ColorData
+        private void AlignRGBD()
+        {
+            // Stores RGB points
+            _Mapper.MapDepthFrameToColorSpace( DepthData, ColorPoints );
+        }
+
+        // Gets 3D points [units = meters] and calculated pf vector
+        private void PreparePointCloud()
+        {
+            boundingbox = findBoundingBoxFromCurrentState();
+
+            // Stores 3D point cloud
+            _Mapper.MapDepthFrameToCameraSpace( DepthData, Camera3DPoints );
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+
+                    int idx = i * Width + j;
+
+                    // if point is not in the bounding box
+                    if (j < boundingbox.x || j >= boundingbox.z || i < boundingbox.y || i >= boundingbox.w)
+                    {
+                        // mark as not useful
+                        Camera3DPoints[idx].X = 0;
+                        Camera3DPoints[idx].Y = 0;
+                        Camera3DPoints[idx].Z = 0;
+                        PfVec[idx] = -1;
+                    }
+                    else
+                    {
+                        Color pixel = ColorTexture.GetPixel( ColorPoints[idx].X, ColorPoints[idx].Y );
+                        // calculate pf
+                        PfVec[idx] = GetPf( pixel );
+                    }
+                }
+            }
+        }
+
+        private void DownsampleImage()
+        {
+        }
+
+        private void GetPf( Color pixel )
+        {
+            int noBins = histogram.BinsNumber;
+            int ru = pixel.r / noBins;
+            int gu = pixel.g / noBins;
+            int bu = pixel.b / noBins;
+            int pidx = ru * noBins * noBins + gu * noBins + bu;
+            return histogram[pidx];
+        }
+
+
+        // TODO
+        private void findBoundingBoxFromCurrentState()
+        {
+        }
+
+        public void ComputePfImageFromHistogram()
+        {
+
+            int noBins = histogram.BinsNumber;
+            float pf = 0;
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+
+                    int idx = i * Width + Height;
+                    Color pixel = ColorTexture.GetPixel( ColorPoints[idx].X, ColorPoints[idx].Y );
+                    pf = GetPf( pixel );
+                    if (pf > 0.5f)
+                    {
+                        PfImage.SetPixel( j, i, Color.red );
+                    }
+                    else if (pf == 0.5f)
+                    {
+                        PfImage.SetPixel( j, i, Color.blue );
+                    }
+                }
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            if (_Reader != null)
+            {
+                _Reader.Dispose();
+                _Reader = null;
+            }
+
+            if (_Sensor != null)
+            {
+                if (_Sensor.IsOpen)
+                {
+                    _Sensor.Close();
+                }
+
+                _Sensor = null;
+            }
+        }
 
     }
 }
